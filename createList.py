0e4b15fbada3c79d88ac832fecb8895eb8258f22
bibekshah220 <bibekshah425@gmail.com>
2025-11-26 21:19:43 +0545

    create list

---
+++ createList.py
@@ -0,0 +1,13 @@
+""""
+What are the way to create a list?
+1. Using squre brackets
+2. using list() constructor
+"""
+# define the list Using squre brackets
+
+list1 = ["A","B","C","D","E"]
+print(list1)
+
+# using list constructor to create a list 
+list2 = list(("A","B","C","D","E"))
+print(list2)
